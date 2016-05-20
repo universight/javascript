bde59d3c4b18528c7b4963dacae5ded1d5e87fd8
universight <universight@gmail.com>
2016-05-20 14:40:56 +0700

    them Slider cua WOW lan 2

---
+++ Slide cua WOW/add.js
@@ -0,0 +1,22 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-25854704-1']);
+_gaq.push(['_setDomainName', '.wowslider.com']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setAllowHash', false]);
+if(document.cookie.match("(^|;\\s)__utma") && !/utmcsr=\(direct\)/.test(unescape(document.cookie))) {
+    _gaq.push(
+      ['_setReferrerOverride', ''],
+      ['_setCampNameKey', 'aaan'], 
+      ['_setCampMediumKey', 'aaam'], 
+      ['_setCampSourceKey', 'aaas'], 
+      ['_setCampTermKey', 'aaat'], 
+      ['_setCampContentKey', 'aaac'], 
+      ['_setCampCIdKey', 'aaaci']
+    )
+}
+_gaq.push(['_trackPageview']);  
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
