cac94420704dfaf1c85bb7a46b136bf033860f29
universight <universight@gmail.com>
2016-07-06 07:27:49 +0700

    Petrophassa add

---
+++ Petrophassa/cung-chuyen-muc.js
@@ -0,0 +1,12 @@
+<!--========CUNG CHUYEN MUC ==============-->
+<script type="text/javascript">
+//<![CDATA[
+    $(function(){ 
+        var MetaCate = $('.postmeta').text(); 
+                });
+    var ThoiSuCate = "THỜI☆SỰ"
+    if (MetaCate == ThoiSuCate) {
+        $('#same-cate').load('http://petrophassa-inc.jimdo.com/thoi-su-mot-sao .j-blogSelectionn'); 
+        }
+//]]>
+</script>
